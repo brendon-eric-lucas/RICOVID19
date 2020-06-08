14a03647cda0f50e690409cbdf6fa8e4db17b027
Brendon Eric Lucas <brendon.eric.lucas@gmail.com>
2020-06-08 19:10:54 -0400

    Added navlistPanel; added tabPanel for testing data

--- app.R
+++ app.R
@@ -9,15 +9,31 @@ dat <- read_sheet(ss, sheet = "Trends")
 ui <- fluidPage(
   
   # App title ----
-  titlePanel(h1("Rhode Island COVID-19 Tracker", align="center")),
+  titlePanel(h3("Rhode Island COVID-19 Tracker")),
   
   navlistPanel(
     "Testing Data",
-    tabPanel("New Positive Labs"),
-    tabPanel("Total Positive Labs"),
-    tabPanel("New Negative Labs"),
-    tabPanel("Total Negative Labs"),
-    tabPanel("Total Tested"),
+    
+    tabPanel("New Positive Labs", 
+              mainPanel(plotOutput("New Positive Labs")) 
+            ),
+    
+    tabPanel("Total Positive Labs", 
+              mainPanel(plotOutput("Total Positive Labs"))
+             ),
+    
+    tabPanel("New Negative Labs", 
+              mainPanel(plotOutput("New Negative Labs"))
+             ),
+    
+    tabPanel("Total Negative Labs", 
+              mainPanel(plotOutput("Total Negative Labs"))
+             ),
+    
+    tabPanel("Total Tested",
+              mainPanel(plotOutput("Total Tested"))
+             ),
+    
     "Hospital Data",
     tabPanel("New Hospital Admissions"),
     tabPanel("Cumulative Hospital Admissions"),
@@ -27,13 +43,17 @@ ui <- fluidPage(
     tabPanel("Deaths"),
     tabPanel("Total Deaths")
   )
-  
-  
 )
 
 server <- function(input, output) {
   
+  data <- reactive({dat})
+  
+  output$"New Positive Labs" <- renderPlot({qplot(data()$Date, data()$`New positive labs`, geom="line")})
+  output$"Total Positive Labs" <- renderPlot({qplot(data()$Date, data()$`Total positive labs`, geom="line")})
+  output$"New Negative Labs" <- renderPlot({qplot(data()$Date, data()$`New negative labs`, geom="line")})
+  output$"Total Negative Labs" <- renderPlot({qplot(data()$Date, data()$`Total negative labs`, geom="line")})
+  output$"Total Tested" <- renderPlot({qplot(data()$Date, data()$`Total tested`, geom="line")})
 }
-
 
 shinyApp(ui = ui, server = server)
