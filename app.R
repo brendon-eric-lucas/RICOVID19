a87f4b013c26abe0b04ae41c63ed908a5c65dd4a
Brendon Eric Lucas <brendon.eric.lucas@gmail.com>
2020-06-09 12:06:30 -0400

    qplot replaced everywhere with ggplot; y-axis text formatted

--- app.R
+++ app.R
@@ -9,11 +9,15 @@ ss <- "https://docs.google.com/spreadsheets/d/1n-zMS9Al94CPj_Tc3K7Adin-tN9x1RSjj
 # render google sheet as dataframe
 dat <- read_sheet(ss, sheet = "Trends")
 
-# rename total columns
+# rename columns
+names(dat)[names(dat) == 'New positive labs'] <- 'new_positive_labs'
 names(dat)[names(dat) == 'Total positive labs'] <- 'total_positive_labs'
+names(dat)[names(dat) == 'New negative labs'] <- 'new_negative_labs'
 names(dat)[names(dat) == 'Total negative labs'] <- 'total_negative_labs'
 names(dat)[names(dat) == 'Total tested'] <- 'total_tested'
+names(dat)[names(dat) == 'New hospital admissions'] <- 'new_hospital_admits'
 names(dat)[names(dat) == 'Cumulative hospital admissions'] <- 'cumulative_hospital_admits'
+names(dat)[names(dat) == 'New hospital discharges'] <- 'new_hospital_discharges'
 names(dat)[names(dat) == 'Cumulative hospital discharges'] <- 'cumulative_hospital_discharges'
 names(dat)[names(dat) == 'Total deaths'] <- 'total_deaths'
 
@@ -81,20 +85,20 @@ server <- function(input, output) {
   data <- reactive({dat})
   
   # plotting for testing data
-  output$"New Positive Labs" <- renderPlot({qplot(data()$Date, data()$`New positive labs`, geom="line")})
-  output$"Total Positive Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=total_positive_labs)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
-  output$"New Negative Labs" <- renderPlot({qplot(data()$Date, data()$`New negative labs`, geom="line")})
-  output$"Total Negative Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=total_negative_labs)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
-  output$"Total Tested" <- renderPlot({ggplot(data(), aes(x=Date, y=total_tested)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
+  output$"New Positive Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=new_positive_labs)) + geom_line() + labs(x = "Date", y = "New Positive Labs")})
+  output$"Total Positive Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=total_positive_labs)) + geom_area(fill="blue", alpha=0.2) + geom_line() + labs(x = "Date", y = "Total Positive Labs")})
+  output$"New Negative Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=new_negative_labs)) + geom_line() + labs(x = "Date", y = "New Negative Labs")})
+  output$"Total Negative Labs" <- renderPlot({ggplot(data(), aes(x=Date, y=total_negative_labs)) + geom_area(fill="blue", alpha=0.2) + geom_line() + labs(x = "Date", y = "Total Negative Labs")})
+  output$"Total Tested" <- renderPlot({ggplot(data(), aes(x=Date, y=total_tested)) + geom_area(fill="blue", alpha=0.2) + geom_line() + labs(x = "Date", y = "Total Tested")})
     
   # plotting for hospital data
-  output$"New Hospital Admissions" <- renderPlot({qplot(data()$Date, data()$`New hospital admissions`, geom="line")})
-  output$"Cumulative Hospital Admissions" <- renderPlot({ggplot(data(), aes(x=Date, y=cumulative_hospital_admits)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
-  output$"New Hospital Discharges" <- renderPlot({qplot(data()$Date, data()$`New hospital discharges`, geom="line")})
-  output$"Cumulative Hospital Discharges" <- renderPlot({ggplot(data(), aes(x=Date, y=cumulative_hospital_discharges)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
+  output$"New Hospital Admissions" <-renderPlot({ggplot(data(), aes(x=Date, y=new_hospital_admits)) + geom_line() + labs(x = "Date", y = "New Hospital Admissions")})
+  output$"Cumulative Hospital Admissions" <- renderPlot({ggplot(data(), aes(x=Date, y=cumulative_hospital_admits)) + geom_area(fill="blue", alpha=0.2) + geom_line() + labs(x = "Date", y = "Cumulative Hospital Admissions")})
+  output$"New Hospital Discharges" <- renderPlot({ggplot(data(), aes(x=Date, y=new_hospital_discharges)) + geom_line() + labs(x = "Date", y = "New Hospital Discharges")})
+  output$"Cumulative Hospital Discharges" <- renderPlot({ggplot(data(), aes(x=Date, y=cumulative_hospital_discharges)) + geom_area(fill="blue", alpha=0.2) + geom_line() + labs(x = "Date", y = "Cumulative Hospital Discharges")})
   
   # plotting for mortality data
-  output$"Deaths" <- renderPlot({qplot(data()$Date, data()$`Deaths`, geom="line")})
+  output$"Deaths" <- renderPlot({ggplot(data(), aes(x=Date, y=Deaths)) + geom_line()})
   output$"Total Deaths" <- renderPlot({ggplot(data(), aes(x=Date, y=total_deaths)) + geom_area(fill="blue", alpha=0.2) + geom_line()})
 }
 
